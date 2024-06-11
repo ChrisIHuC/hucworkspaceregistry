f86dfa7a4257ba31bdc62e1785128b539e185e17
ChrisIHuC <128488640+ChrisIHuC@users.noreply.github.com>
2024-06-11 14:43:30 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'HuC Kasm Workspace Registry',
+    description: 'Kasm Workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://chrisihuc.github.io/hucworkspaceregistry/1.0/',
+    contactUrl: 'https://di.huc.knaw.nl/',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/hucworkspaceregistry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
